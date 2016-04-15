aaa0f9f67066a1c6c48173dc0ad51dd8043c3f3e
Kyle Anderson <kanderson@mosesinc.com>
2016-04-15 11:11:45 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    modernizr = require('gulp-modernizr');
+
+var Elixir = require('laravel-elixir')
+    Task = Elixir.Task,
+    $ = Elixir.Plugins,
+    config = Elixir.config;
+
+Elixir.extend('modernizr', function(src, output, options) {
+  var paths = new Elixir.GulpPaths()
+    .src(src || [config.get('public.css.outputFolder') + '/**/*.css', config.get('public.js.outputFolder') + '/**/*.js'])
+    .output(output || config.get('public.js.outputFolder') + '/modernizr.js');
+
+  new Task('modernizr', function() {
+    this.log(paths.src, paths.output);
+
+    paths.src.path.push('!' + paths.output.path);
+
+    return gulp
+      .src(paths.src.path)
+      .pipe(modernizr(paths.output.name, options || {}))
+      .pipe($.if(config.production, $.uglify()))
+      .pipe(gulp.dest(paths.output.baseDir));
+  });
+});
